d5ae9a7cbbcffc99525374e83d1f72f6856ff8e9
Logan Gaillard <logangaillard1@gmail.com>
2024-09-25 11:23:06 +0200

    First commit

---
+++ vue/defaultContent/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moto GP 24 <?php isset($page) ? " - $page" : "" ?></title>
+    <script src="https://cdn.tailwindcss.com"></script><!-- Tailwind CSS FRAMEWORK CSS-->
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <img id="logo" src="../img/moto-gp-logo.webp">
+        </div>
+    </header>
