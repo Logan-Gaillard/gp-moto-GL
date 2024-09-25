93c126d3b473f0796699c5c750dbfe4b5da1a5fd
Logan Gaillard <logangaillard1@gmail.com>
2024-09-25 11:38:22 +0200

    Final update

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,9 +1,4 @@
 <link rel="stylesheet" href="css/accueil.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
-
-<div data-depth="0.2" class="fullscreen background">
-    <div class="fullscreen blurify"></div>
-</div>
 
 <div class="">
 </div>
