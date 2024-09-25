df01ad1f4dfedad346345a296f8c7a12cd34b0ec
Logan Gaillard <logangaillard1@gmail.com>
2024-09-25 11:35:54 +0200

    Update

--- vue/defaultContent/header.php
+++ vue/defaultContent/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Moto GP 24 <?php isset($page) ? " - $page" : "" ?></title>
+    <link rel="icon" href="../img/icon.ico" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script><!-- Tailwind CSS FRAMEWORK CSS-->
     <link rel="stylesheet" href="css/index.css">
 </head>
